bd71b027d3fc911aaa6b5e81b69d20038631d3c7
Clif Wolfe <34320249+flounderK@users.noreply.github.com>
2018-06-01 14:02:37 -0400

    Added ListProcess csharp and buildfile

---
+++ ListProcesses/ListProcesses.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Management;
+using System.Collections.Generic;
+using Microsoft.Management.Infrastructure;
+using Microsoft.Management.Infrastructure.Options;
+
+namespace MyService
+{
+	class MyS 
+	{	
+		[STAThread]
+		static void Main()
+		{
+			string Namespace = @"root\cimv2";
+			string ComputerName = "127.0.0.1";
+			string ClassName = "Win32_Process";
+			DComSessionOptions DComOptions = new DComSessionOptions();
+			DComOptions.Impersonation = ImpersonationType.Impersonate;
+
+			CimSession Session = CimSession.Create(ComputerName, DComOptions);
+			IEnumerable<CimInstance> processes = Session.EnumerateInstances(Namespace, ClassName);
+			foreach (CimInstance process in processes)
+			{
+				Console.WriteLine("{0} {1} {2}",
+								  process.CimInstanceProperties["Name"],
+				                  process.CimInstanceProperties["ParentProcessId"],
+			                      process.CimInstanceProperties["ProcessId"],
+								  process.CimInstanceProperties["CommandLine"]);
+			}
+			Console.ReadLine();
+		}
+	}
+}
